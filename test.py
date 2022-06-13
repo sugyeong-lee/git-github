cda39c53dd2153bf027416cec06748e293dc8dee
sugyeong-lee <101383749+sugyeong-lee@users.noreply.github.com>
2022-06-13 22:07:59 +0900

    깃허브 내용 추가

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
 print("hello world!")
 print("드디어 로컬과 연결 성공!!!!")
 
-# git clone""
-# git add --all
-# git commit -m "커밋할 내용"
-# git push origin main
-# reset -all
+
+# 깃허브 연결 확인
+git remote -v
+# 저장소 끊기
+git remote remove origin
+# 저장소 연결
+git remote add origin " "  (shift+insert)
+# 저장소 기존 내용 가져오기
+git clone""
+# 전체 스테이지 올리기
+git add .
+# 커밋+ 메세지
+git commit -m "커밋할 내용"
+# 깃허브에 추가하기
+git push origin main
